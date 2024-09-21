ec0c8d5b0acf811a7a39b65add0942d64448e44d
haedarrfd <hroofid@gmail.com>
2024-09-21 20:53:08 +0700

    set up environment

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("e")
+	fmt.Println("Hello")
+
+	// Setting up the environment
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the env")
+	}
+
+	fmt.Println(portString)
 }
